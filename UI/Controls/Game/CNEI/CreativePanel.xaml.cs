79af5a016f48b280d350b688b849b817c6beb126
Djekke <djekke@gmail.com>
2018-09-19 19:03:00 +0300

    Move Creative Side Panel to separate xaml. Add Heal button.

---
+++ UI/Controls/Game/CNEI/CreativePanel.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AtomicTorch.CBND.CNEI.UI.Controls.Game.CNEI
+{
+    using AtomicTorch.CBND.CNEI.UI.Controls.Game.CNEI.Data;
+    using AtomicTorch.GameEngine.Common.Client.MonoGame.UI;
+
+    public partial class CreativePanel : BaseUserControl
+    {
+        private ViewModelCreativePanel viewModel;
+
+        public CreativePanel()
+        {
+
+        }
+
+        protected override void InitControl()
+        {
+            this.DataContext = this.viewModel = new ViewModelCreativePanel();
+        }
+
+        protected override void DisposeControl()
+        {
+            this.DataContext = null;
+            this.viewModel.Dispose();
+            this.viewModel = null;
+        }
+    }
+}
---
+++ UI/Controls/Game/CNEI/Data/ViewModelCreativePanel.cs
@@ -0,0 +1,17 @@
+﻿namespace AtomicTorch.CBND.CNEI.UI.Controls.Game.CNEI.Data
+{
+    using AtomicTorch.CBND.CoreMod.Systems.Console;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Core;
+    using AtomicTorch.GameEngine.Common.Client.MonoGame.UI;
+
+    public class ViewModelCreativePanel : BaseViewModel
+    {
+        public BaseCommand Heal { get; }
+
+        public ViewModelCreativePanel()
+        {
+            this.Heal = new ActionCommand(() => 
+                                ConsoleCommandsSystem.SharedExecuteConsoleCommand("/player.heal"));
+        }
+    }
+}
