2f75ca585e2d433675f8289d26444a580df1bb63
Djekke <djekke@gmail.com>
2019-01-29 14:56:15 +0300

    Add hud button dynamically. Core mod files are not overriden.

---
+++ UI/Controls/Game/CNEImenu/Data/ViewModelHUDButton.cs
@@ -0,0 +1,28 @@
+﻿namespace CryoFall.CNEI.UI.Controls.Game.CNEImenu.Data
+{
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Core;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Core.Menu;
+    using CryoFall.CNEI.ClientComponents.Input;
+    using CryoFall.CNEI.UI.Controls.Game.CNEImenu.Controls;
+
+    public class ViewModelHUDButton: BaseViewModel
+    {
+        public Menu MenuCNEI { get; }
+
+        public IButtonReference ButtonReference { get; set; }
+
+        public ViewModelHUDButton()
+        {
+            MenuCNEI = Menu.Register<WindowCNEImenu>();
+            ButtonReference = new CNEIbuttonReference() { Button = CNEIbutton.MenuOpen };
+        }
+
+        protected override void DisposeViewModel()
+        {
+            base.DisposeViewModel();
+
+            MenuCNEI?.Dispose();
+            ButtonReference = null;
+        }
+    }
+}
