4228af6c9f7938b55bd738051e0a1d97eb470b6d
Djekke <djekke@gmail.com>
2019-03-09 04:25:28 +0300

    Fix new API changes on A19.

--- Scripts/ClientComponents/Input/CNEIbutton.cs
+++ Scripts/ClientComponents/Input/CNEIbutton.cs
@@ -3,29 +3,21 @@ namespace CryoFall.CNEI.ClientComponents.Input
     using AtomicTorch.CBND.CoreMod.ClientComponents.Input;
     using AtomicTorch.CBND.GameApi;
     using AtomicTorch.CBND.GameApi.ServicesClient;
+    using System.ComponentModel;
 
     [NotPersistent]
     public enum CNEIbutton
     {
-        [ButtonInfo(
-            "Open CNEI",
-            "Open CryoFall NEI",
-            InputKey.N,
-            Category = "CNEI")]
+        [Description("Open CNEI")]
+        [ButtonInfo(InputKey.N, Category = "CNEI")]
         MenuOpen,
 
-        [ButtonInfo(
-            "Show item info",
-            "Open CryoFall NEI Details window with information about item under mouse coursor",
-            InputKey.I,
-            Category = "CNEI")]
+        [Description("Show item info")]
+        [ButtonInfo(InputKey.I, Category = "CNEI")]
         MenuDetails,
 
-        [ButtonInfo(
-            "Previous info",
-            "Show previous information in Details window or close it",
-            InputKey.Back,
-            Category = "CNEI")]
+        [Description("Previous info")]
+        [ButtonInfo(InputKey.Back, Category = "CNEI")]
         MenuBack,
     }
 }
