2d6f6cc25d17c2bc05b57236a03f2230f299bab7
Djekke <djekke@gmail.com>
2019-04-09 10:27:21 +0300

    Add view model for floor.

---
+++ UI/Controls/Game/CNEImenu/Data/EntityTemplates/ProtoObjectFloorViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace CryoFall.CNEI.UI.Controls.Game.CNEImenu.Data
+{
+    using AtomicTorch.CBND.CoreMod.StaticObjects.Structures.Floors;
+    using CryoFall.CNEI.UI.Controls.Game.CNEImenu.Managers;
+    using JetBrains.Annotations;
+
+    public class ProtoObjectFloorViewModel : ProtoObjectStructureViewModel
+    {
+        public ProtoObjectFloorViewModel([NotNull] IProtoObjectFloor floor) : base(floor)
+        {
+        }
+
+        /// <summary>
+        /// Initilize information about entity - invoked after all entity view Models created,
+        /// so you can use links to other entity by using <see cref="EntityViewModelsManager.GetEntityViewModel" />
+        /// and <see cref="EntityViewModelsManager.GetAllEntityViewModels" />.
+        /// </summary>
+        public override void InitInformation()
+        {
+            base.InitInformation();
+
+            if (ProtoEntity is IProtoObjectFloor floor)
+            {
+                EntityInformation.Add(new ViewModelEntityInformation("Move speed multiplier",
+                    floor.CharacterMoveSpeedMultiplier));
+            }
+        }
+    }
+}
