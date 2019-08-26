d48499e1aa20e09c73817fbd1a07ee8e77dcee4c
Djekke <djekke@gmail.com>
2019-08-26 03:17:29 +0300

    Add new recipe type (seed -> plant).

--- UI/CNEI/Data/EntityTemplates/Items/ProtoItemSeedViewModel.cs
+++ UI/CNEI/Data/EntityTemplates/Items/ProtoItemSeedViewModel.cs
@@ -11,6 +11,23 @@
         }
 
         /// <summary>
+        /// Initilize entity reletionships with each other - invoked after all entity view Models created,
+        /// so you can access them by using <see cref="EntityViewModelsManager.GetEntityViewModel" />
+        /// and <see cref="EntityViewModelsManager.GetAllEntityViewModels" />.
+        /// </summary>
+        public override void InitAdditionalRecipes()
+        {
+            base.InitAdditionalRecipes();
+
+            if (ProtoEntity is IProtoItemSeed seed)
+            {
+                PlantRecipe = new PlantRecipeViewModel(this,
+                    EntityViewModelsManager.GetEntityViewModel(seed.ObjectPlantProto));
+                EntityViewModelsManager.AddRecipe(PlantRecipe);
+            }
+        }
+
+        /// <summary>
         /// Initilize information about entity - invoked after all entity view Models created,
         /// so you can use links to other entity by using <see cref="EntityViewModelsManager.GetEntityViewModel" />
         /// and <see cref="EntityViewModelsManager.GetAllEntityViewModels" />.
@@ -25,5 +42,7 @@
                     EntityViewModelsManager.GetEntityViewModel(seed.ObjectPlantProto)));
             }
         }
+
+        public RecipeViewModel PlantRecipe { get; set; }
     }
 }
---
+++ UI/CNEI/Data/EntityTemplates/Recipes/PlantRecipeViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace CryoFall.CNEI.UI.Data
+{
+    using System;
+    using CryoFall.CNEI.Managers;
+    using JetBrains.Annotations;
+
+    public class PlantRecipeViewModel : RecipeViewModel
+    {
+        public override string ResourceDictionaryName => "PlantRecipeDataTemplate.xaml";
+
+        public override string RecipeTypeName => "Plant growth";
+
+        /// <summary>
+        /// Constructor for entity with droplist.
+        /// Must be used only in InitEntityRelationships phase.
+        /// </summary>
+        /// <param name="seedEntityViewModel">View Model of seed entity.</param>
+        /// <param name="plantEntityViewModel">View Model of grown plant entity.</param>
+        public PlantRecipeViewModel([NotNull] ProtoEntityViewModel seedEntityViewModel,
+            [NotNull] ProtoEntityViewModel plantEntityViewModel)
+            : base(seedEntityViewModel.ProtoEntity)
+        {
+            if (!EntityViewModelsManager.EntityDictonaryCreated)
+            {
+                throw new Exception("CNEI: Droplist constructor used before all entity VMs sets.");
+            }
+
+            InputItemsList.Add(seedEntityViewModel);
+
+            SeedEntity = seedEntityViewModel;
+
+            OutputItemsList.Add(plantEntityViewModel);
+
+            PlantEntity = plantEntityViewModel;
+        }
+
+        public ProtoEntityViewModel SeedEntity { get; protected set; }
+
+        public ProtoEntityViewModel PlantEntity { get; protected set; }
+    }
+}
