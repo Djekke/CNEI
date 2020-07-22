246f67c03420a4043bb9b85cbd60c53aaa6ef9cd
djekke <djekke@gmail.com>
2020-07-22 04:37:42 +0300

    0.4.7 (Move version up for new release)

--- Scripts/CNEI/Managers/EntityViewModelsManager.cs
+++ Scripts/CNEI/Managers/EntityViewModelsManager.cs
@@ -90,7 +90,7 @@
         /// </summary>
         public static bool EntityDictionaryCreated = false;
 
-        public static Version CurrentVersion => new Version("0.4.6");
+        public static Version CurrentVersion => new Version("0.4.7");
 
         public static Version VersionFromClientStorage = null;
 
